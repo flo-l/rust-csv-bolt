caa1d89ca5d23a3bffa740d46139e6fbb54bcf8d
Florian Lackner <florian.lackner@gmail.com>
2018-10-11 09:29:03 +0200

    pull in memchr crate add seperators

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,31 @@
 #[cfg(test)]
 extern crate itertools;
 
+extern crate memchr;
+
+use memchr::memchr3;
+
 #[cfg(test)]
 mod tests;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Csv<'a> {
     data: &'a [u8],
+    row_sep: u8,
+    col_sep: u8,
+    quote: u8,
+    escape: u8,
 }
 
 impl<'a> Csv<'a> {
     pub fn from_bytes(bytes: &'a [u8]) -> Self {
-        Csv { data: bytes }
+        Csv {
+            data: bytes,
+            row_sep: b'\n',
+            col_sep: b',',
+            quote: b'"',
+            escape: b'\\',
+        }
     }
 }
 
