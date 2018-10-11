cedbdb8cc5c5cad1307012c73750a383bdc251be
Florian Lackner <florian.lackner@gmail.com>
2018-10-11 09:17:50 +0200

    add basic api stubs and a test for the stubs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,42 @@
 #[cfg(test)]
+extern crate itertools;
+
+#[cfg(test)]
 mod tests;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Csv<'a> {
+    data: &'a [u8],
+}
+
+impl<'a> Csv<'a> {
+    pub fn from_bytes(bytes: &'a [u8]) -> Self {
+        Csv { data: bytes }
+    }
+}
+
+impl<'a> Iterator for Csv<'a> {
+    type Item = RowIter<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unimplemented!();
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct RowIter<'a> {
+    row: &'a [u8],
+}
+
+impl<'a> Iterator for RowIter<'a> {
+    type Item = Cell<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unimplemented!();
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Cell<'a> {
+    raw: &'a [u8],
+}
--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,28 @@
+use itertools::Itertools;
+use std::iter::repeat;
+use Csv;
+
+fn create_buffer(row_count: usize, col_count: usize, cell_count: usize) -> Vec<u8> {
+    static COL_SEP: &[u8] = b",";
+    static ROW_SEP: &[u8] = b"\n";
+    static CELL_CONTENT: u8 = b'X';
+
+    let row_seps = repeat(ROW_SEP).take(row_count - 1);
+    let col_seps = repeat(COL_SEP).take(col_count - 1);
+
+    let cell: Vec<u8> = repeat(CELL_CONTENT).take(cell_count).collect();
+    let row:  Vec<u8> = Itertools::flatten(repeat(&*cell).interleave_shortest(col_seps)).cloned().collect();
+    let csv:  Vec<u8> = Itertools::flatten(repeat(&*row).interleave_shortest(row_seps)).cloned().collect();
+
+    csv
+}
+
 #[test]
-fn it_works() {
-    assert_eq!(2 + 2, 4);
+fn basic_api() {
+    let csv = create_buffer(20,20,100);
+    for row in Csv::from_bytes(&*csv) {
+        for cell in row {
+            // do something
+        }
+    }
 }
